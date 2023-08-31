f13bf206016f2bfbd3d7e4c6c8de0a1f8370389d
NaujacGyt <naujacgamermcpe@gmail.com>
2023-08-30 23:30:15 -0500

    estructuracion proyecto cajero

--- cajero.py
+++ cajero.py
@@ -1,3 +1,9 @@
-saludo = "Hola Mundo"
-
-print(saludo)
+saldo_disponible = 1000 ##Saldo Disponible en la Cuenta
+valor_a_retirar = input("ingrese la cantidad a retirar:")
+  
+if (valor_a_retirar <= saldo_disponible):
+    print("retiro exitoso")
+elif(valor_a_retirar > saldo_disponible):
+    print(saldo insuficiente)
+else:
+    print(input("desea ver su saldo? Y or N :"))
