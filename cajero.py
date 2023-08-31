ba14a4136eb78cf6e7128e263e550c9d21c472bf
NaujacGyt <naujacgamermcpe@gmail.com>
2023-08-31 00:14:08 -0500

    avances del proyecto principal  V: 0.1

--- cajero.py
+++ cajero.py
@@ -1,9 +1,31 @@
 saldo_disponible = 1000 ##Saldo Disponible en la Cuenta
-valor_a_retirar = input("ingrese la cantidad a retirar:")
-  
-if (valor_a_retirar <= saldo_disponible):
-    print("retiro exitoso")
-elif(valor_a_retirar > saldo_disponible):
-    print(saldo insuficiente)
-else:
-    print(input("desea ver su saldo? Y or N :"))
+
+accion = input("que desea hacer 1.-retirar 2.-consignar 3.-Consultar Saldo : ")
+
+#valor_a_consignar = input("ingrese la cantidad a consignar sin puntos ni espacios")    
+
+#valor_a_retirar = input("ingrese la cantidad a retirar:")
+
+accion1 = int(accion) 
+
+if (accion1 == 1):
+    print(input("ingrese la cantidad a retirar:"))
+
+elif (accion1 == 2):
+    print (input("ingrese la cantidad a consignar sin puntos ni espacios"))
+
+elif (accion1 == 3):
+    print ("$",saldo_disponible, "<----- este es su saldo disponible.")
+
+elif (accion1 >= 4):
+    print("Error digite uno de los numeros indicados en el menu principal.", accion)
+
+#valor_a_consignar = input("ingrese la cantidad a consignar sin puntos ni espacios")    
+
+#valor_a_retirar = input("ingrese la cantidad a retirar:")
+#cantidad = int(valor_a_retirar) 
+#if (cantidad <= saldo_disponible):
+   # print("retiro exitoso")
+#elif(cantidad > saldo_disponible):
+   # print("saldo insuficiente")
+
