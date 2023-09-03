115e5a7a4810af20d63f8fd6ef42dfbee98ed1fe
NaujacGyt <naujacgamermcpe@gmail.com>
2023-09-03 16:47:54 -0500

    se agrega el bucle para la reproduccion del menú

--- cajero.py
+++ cajero.py
@@ -2,20 +2,22 @@
 
 saldo_disponible = 1000 ##Saldo Disponible en la Cuenta
 saldo_disponible=int(saldo_disponible)
-print("BIENVENIDO A SU CAJERO AUTOMATICO")
+accion = 0
+print("===================================\n\
+BIENVENIDO A SU CAJERO AUTOMATICO\n\
+===================================")
 
-accion = int(input("que desea hacer \n\
+while True:
+
+ accion = int(input("que desea hacer \n\
                1.-RETIRAR \n\
                2.-CONSIGNAR \n\
                3.-CONSULTAR SALDO \n\
                4.-SALIR \n\
                DIGITE AQUI SU ELECCION: "))
 
-#valor_a_consignar = input("ingrese la cantidad a consignar sin puntos ni espacios")    
 
-#valor_a_retirar = input("ingrese la cantidad a retirar:")
-control = 0
-if (accion ==1):
+ if (accion ==1):
     retirar = int(input("INGRESE LA CANTIDAD A RETIRAR: "))
     #print(retirar,"25")
     if (retirar <= saldo_disponible):
@@ -31,7 +33,9 @@ if (accion ==1):
             
         else:
             print("GRACIAS POR UTILIZAR NUESTROS SERVICIOS =)")
-elif(accion == 2):
+    else:
+       print("ERROR ACCION NO VALIDA")
+ elif(accion == 2):
     consignar = int(input("INGRESE EL MONTO A CONSIGNAR: "))
     if(consignar > 0):
         accionconsig = int(input("ACCION REALIZADA CON EXITO\n\
@@ -44,13 +48,15 @@ elif(accion == 2):
             print("SU NUEVO SALDO ES", "$",saldo_disponible)
         else:
             print("GRACIAS POR UTILIZAR NUESTROS SERVICIOS")
-elif(accion == 3):
+ elif(accion == 3):
     print("SU SALDO DISPONIBLE ES: ", "$", saldo_disponible)
 
-elif(accion == 4):
+ elif(accion == 4):
      print('=======================================')
-     print('GRACIAS POR USAR NUESTROS SERVICIOS!')
+     print('GRACIAS POR USAR NUESTROS SERVICIOS!\n\
+           VUELVA PRONTO')
      print('=======================================')
-     
-else: 
+     break
+ else: 
     print("ACCION INCORRECTA, POR FAVOR VERIFIQUE SU ELECCION Y VUELVA A INTENTAR")
+
